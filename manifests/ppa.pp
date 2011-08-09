88fa083eae854b11e342b4eb7beff6c98b335644
jloope <jloope@janrain.com>
2011-08-09 10:25:36 -0700

    rearrange the negation

--- manifests/ppa.pp
+++ manifests/ppa.pp
@@ -12,7 +12,7 @@ define apt::ppa(
 
     exec { "add-apt-repository-${name}":
         command => "/usr/bin/add-apt-repository ${name}",
-        onlyif => "! test -f /etc/apt/sources.list.d/nagiosinc-ppa-$lsbdistcodename.list",
+        onlyif => "test ! -f /etc/apt/sources.list.d/nagiosinc-ppa-$lsbdistcodename.list",
         notify  => Exec["apt-update-${name}"],
     }
 }
