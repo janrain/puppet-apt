d0b564fd52e480bd885363dc24754e03ac16f9bf
jloope <jloope@janrain.com>
2011-08-15 16:32:53 -0700

    change that to an unless

--- manifests/ppa.pp
+++ manifests/ppa.pp
@@ -12,7 +12,7 @@ define apt::ppa(
 
     exec { "add-apt-repository-${name}":
         command => "/usr/bin/add-apt-repository ${name}",
-        onlyif => "grep -q `echo ${name} | sed 's;\(.*:\)\(.*\);\2;g'` /etc/apt/sources.list.d/*.list",
+        unless => "grep -q `echo ${name} | sed 's;\(.*:\)\(.*\);\2;g'` /etc/apt/sources.list.d/*.list",
         notify  => Exec["apt-update-${name}"],
     }
 }
